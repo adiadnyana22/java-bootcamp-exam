24b7a64e6afe5eba289f8da6a4fe5ad4e0bdca2a
Adi Adnyana <adiadnyana22@gmail.com>
2021-05-17 15:48:26 +0800

    Test

---
+++ src/NumberOne/Share.java
@@ -0,0 +1,20 @@
+package NumberOne;
+
+public class Share {
+	int numShare;
+	String username;
+	
+	public Share(int numShare, String username) {
+		this.numShare = numShare;
+		this.username = username;
+	}
+	
+	public static boolean isNumeric(String str) { 
+		try {  
+			Double.parseDouble(str);  
+		    return true;
+		} catch(NumberFormatException e){  
+		    return false;  
+		}  
+	}
+}
